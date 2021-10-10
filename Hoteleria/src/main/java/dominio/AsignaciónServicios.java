5501d1ed03007f0453bc29af332dc6959a7b4b5f
leoneldc <leonelchavez2013@hotmail.com>
2021-10-09 22:59:41 -0600

    Creación de Asignación de Servicios a Habitacion
    Se creo la funcionalidad de insertar, eliminar, cancelar, buscar

---
+++ Hoteleria/src/main/java/dominio/AsignaciónServicios.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author leone
+ */
+public class AsignaciónServicios {
+    private String correlativo, id_tarifa, id_servicio;
+
+    public String getCorrelativo() {
+        return correlativo;
+    }
+
+    public void setCorrelativo(String correlativo) {
+        this.correlativo = correlativo;
+    }
+
+    public String getId_tarifa() {
+        return id_tarifa;
+    }
+
+    public void setId_tarifa(String id_tarifa) {
+        this.id_tarifa = id_tarifa;
+    }
+
+    public String getId_servicio() {
+        return id_servicio;
+    }
+
+    public void setId_servicio(String id_servicio) {
+        this.id_servicio = id_servicio;
+    }
+}
