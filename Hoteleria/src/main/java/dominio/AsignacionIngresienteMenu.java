0e2e885bbc143ea3323f600d452a4d206daa0272
jeffersondavila <jeffersondavila16@gmail.com>
2021-11-10 12:50:50 -0600

    Asignar ingredientes al menu

---
+++ Hoteleria/src/main/java/dominio/AsignacionIngresienteMenu.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author JEFF
+ */
+public class AsignacionIngresienteMenu {
+    String idCorrelativo, idPlato, idProducto, cantidadIngrediente, nombrePlato, unidadPlato;
+
+    public String getIdCorrelativo() {
+        return idCorrelativo;
+    }
+
+    public void setIdCorrelativo(String idCorrelativo) {
+        this.idCorrelativo = idCorrelativo;
+    }
+
+    public String getIdPlato() {
+        return idPlato;
+    }
+
+    public void setIdPlato(String idPlato) {
+        this.idPlato = idPlato;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getCantidadIngrediente() {
+        return cantidadIngrediente;
+    }
+
+    public void setCantidadIngrediente(String cantidadIngrediente) {
+        this.cantidadIngrediente = cantidadIngrediente;
+    }
+
+    public String getNombrePlato() {
+        return nombrePlato;
+    }
+
+    public void setNombrePlato(String nombrePlato) {
+        this.nombrePlato = nombrePlato;
+    }
+
+    public String getUnidadPlato() {
+        return unidadPlato;
+    }
+
+    public void setUnidadPlato(String unidadPlato) {
+        this.unidadPlato = unidadPlato;
+    }
+}
